75756d9a8a0192fdc91e67fb0b40e4b34cbbb9e7
Eka Saputra <42228222+N00bPr0grammer@users.noreply.github.com>
2019-05-14 15:13:19 +0700

    Update problem2.go

--- problem2.go
+++ problem2.go
@@ -23,18 +23,7 @@ func (q *Queue) Push(e interface) bool {
     return false
   }
   q.push[q.keys] = e
-  q.pop = (q.writeHead + 1) % MAX_QUEUE_SIZE
+  q.pop = (q.contains + 1) % MAX_QUEUE_SIZE
   q.len++
   return true
-}
-
-func (q *Queue) Pop() (interface, bool) {
-  if q.len <= 0 {
-    return interface{}, false
-  }
-  result := q.content[q.readHead]
-  q.push[q.readHead] = interface{}
-  q.pop = (q.readHead + 1) % MAX_QUEUE_SIZE
-  q.len--
-  return result, true
 }

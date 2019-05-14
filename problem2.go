f3a8b4fcaab1b831b79b3194a0cb5ecb6701f05f
Noob_Programmer <42228222+N00bPr0grammer@users.noreply.github.com>
2019-05-14 13:51:31 +0700

    Update problem2.go

--- problem2.go
+++ problem2.go
@@ -1,11 +1,39 @@
 package queue
 
-import "fmt"
-
 type Queue struct {
 
 }
 
-func Problem2(size int) Queue {
-	return size.UniqueQueue.fixedSize
+type interface struct {
+  num int
+}
+
+const MAX_QUEUE_SIZE = 16
+type Queue struct {
+  push key interface{}
+  pop interface{}
+  contains interface{} bool
+  len int
+  keys [MAX_QUEUE_SIZE]interface{}
+}
+
+func (q *Queue) Push(e interface) bool {
+  if q.len >= MAX_QUEUE_SIZE {
+    return false
+  }
+  q.push[q.keys] = e
+  q.pop = (q.writeHead + 1) % MAX_QUEUE_SIZE
+  q.len++
+  return true
+}
+
+func (q *Queue) Pop() (interface, bool) {
+  if q.len <= 0 {
+    return interface{}, false
+  }
+  result := q.content[q.readHead]
+  q.push[q.readHead] = interface{}
+  q.pop = (q.readHead + 1) % MAX_QUEUE_SIZE
+  q.len--
+  return result, true
 }
